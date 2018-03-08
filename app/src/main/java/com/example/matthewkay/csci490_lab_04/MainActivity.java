91c2799005ebaedb589353788a9087d4371462d7
Matthew Kay <mkay0961@gmail.com>
2018-03-08 18:01:57 -0500

    webview lab

--- app/src/main/java/com/example/matthewkay/csci490_lab_04/MainActivity.java
+++ app/src/main/java/com/example/matthewkay/csci490_lab_04/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.matthewkay.csci490_lab_04;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView webview = new WebView(this);
+        setContentView(webview);
+        webview.loadUrl("https://github.com/");
+        Log.i("j","hello");
+
+
     }
 }
