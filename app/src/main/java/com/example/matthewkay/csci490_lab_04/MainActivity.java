e4ad63e93ac4afc2a7d183adc739a39fa07855da
Matthew Kay <mkay0961@gmail.com>
2018-03-13 15:15:20 -0400

    Async

--- app/src/main/java/com/example/matthewkay/csci490_lab_04/MainActivity.java
+++ app/src/main/java/com/example/matthewkay/csci490_lab_04/MainActivity.java
@@ -3,68 +3,87 @@ package com.example.matthewkay.csci490_lab_04;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView view;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getURL("https://jsonplaceholder.typicode.com/users");
+
+        TextView view= findViewById(R.id.webview);
+
+        downloadTask task = new downloadTask(view);
+        task.execute("https://jsonplaceholder.typicode.com/users");
+
+
     }
 
 
-    public String getURL(String s)  {
 
 
-
-        URL url = null;
-        try {
-            url = new URL(s);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-
-
-
-        URLConnection connection = null;
-        try {
-            connection = url.openConnection();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        InputStreamReader isr = null;
-        try {
-            isr = new InputStreamReader((connection.getInputStream()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-
-        BufferedReader in = new BufferedReader(isr);
-        String line = null;
-        try {
-            line = in.readLine();
-            while (in.readLine() != null) {
-                Log.i("hello", line);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        return s;
-    }
+//    public String getURL(String s)  {
+//
+//
+//
+//        URL url = null;
+//
+//        try {
+//            url = new URL(s);
+//        } catch (MalformedURLException e) {
+//
+//            e.printStackTrace();
+//        }
+//
+//
+//
+//        URLConnection connection = null;
+//
+//        try {
+//            connection = url.openConnection();
+//        } catch (IOException e) {
+//
+//            e.printStackTrace();
+//        }
+//
+//
+//        InputStreamReader isr = null;
+//
+//
+//        try {
+//            isr = new InputStreamReader((connection.getInputStream()));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//
+//        BufferedReader in = new BufferedReader(isr);
+//        String line = null;
+//        try {
+//            line = in.readLine();
+//            while (in.readLine() != null) {
+//                Log.i("hello", line);
+//            }
+//
+//        } catch (IOException e) {
+//
+//            e.printStackTrace();
+//        }
+//
+//
+//        return s;
+//    }
 }
 
